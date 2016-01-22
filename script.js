9fec24f29798bb02b48f6b9802e575add2422edc
Chris Whong <chris@whong.org>
2016-01-22 00:40:34 -0500

    add children to index.json  files

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
- var path = require('path'),
+var path = require('path'),
   extract = require('pdf-text-extract'),
   fs = require('fs'),
   walk = require('walk');
 
-  // //input file
-  // var filePath = path.join(__dirname, "source/chapter1.pdf");
-  var outputPath = './output';
+var outputPath = '../nyc-construction-codes-data/GENERAL-ADMINISTRATIVE-PROVISIONS';
 
 var files   = [];
+var topIndex = {
+  heading: "GENERAL-ADMINISTRATIVE-PROVISIONS",
+  children: []
+};
 
 // Walker options
 var walker  = walk.walk('./source', { followLinks: false });
-
 
 walker.on('file', function(root, stat, next) {
     // Add this file to the list of files
@@ -20,54 +21,58 @@ walker.on('file', function(root, stat, next) {
 });
 
 walker.on('end', function() {
+
     files.forEach(function( file ) {
       scrape(path.join(__dirname, file))
     })
 });
  
-  //add output directory
-  if (!fs.existsSync(outputPath)){
-    fs.mkdirSync(outputPath);
-  }
-
-
+//scrape the pdf
 function scrape(filePath) {
+  console.log('Scraping ' + filePath + '...');
 
   extract(filePath,  { splitPages: false }, function (err, text) {
     if (err) {
-      console.dir(err)
-      return
+      console.dir(err);
+      return;
     }
 
     //first get the chapter number and heading
 
     var match = text.match(/CHAPTER\D+(\d)\D+\s+([A-Z]+)\s+ARTICLE/);
 
-    var index = {
+    var chapterIndex = {
       prefix: "CHAPTER",
       num: match[1],
-      heading: match[2]
+      heading: match[2],
+      children: []
     }
 
-    var chapterPath = createDirectory(index,outputPath);
+    topIndex.children.push(chapterIndex.prefix + '-' + chapterIndex.num);
+
+    var chapterPath = createDirectory(chapterIndex,outputPath);
 
   
     var articles = text.match(/(ARTICLE[\s\S]*?(?=ARTICLE|$))/g);
 
+
     articles.forEach(function(article) {
-     
+      
 
       //get the article number and heading
       var match = article.match(/ARTICLE\s(\d+)\s+(.*)/);
  
 
-      var index = {
+      var articleIndex = {
         prefix: "ARTICLE",
         num: match[1],
-        heading: match[2]
+        heading: match[2],
+        children: []
       }
 
-      var articlePath = createDirectory(index, chapterPath);
+      chapterIndex.children.push(articleIndex.prefix + '-' + articleIndex.num);
+
+      var articlePath = createDirectory(articleIndex, chapterPath);
 
       var sections = article.match(/(^§[\s\S]*?(?=§|$))|(\s*?§[\s\S]*?(?=§|$))/g);
       if (sections) {
@@ -76,17 +81,30 @@ function scrape(filePath) {
           //get the section number and heading
           var match = section.match(/§([\d-.]+)\s+([\s\S]*?)\.([\s\S]+)/);
 
-          var index = {
+          var sectionIndex = {
             prefix: "SECTION",
             num: match[1],
             heading: match[2],
             text: match[3]
           }
 
-          createSectionFile(index, articlePath);
+          articleIndex.children.push(sectionIndex.num)
+
+          createSectionFile(sectionIndex, articlePath);
         })
+
+        //write article index
+        fs.writeFile(articlePath + '/index.json', JSON.stringify(articleIndex, null, 2));
       } 
     })
+
+    //write chapter index
+    fs.writeFile(chapterPath + '/index.json', JSON.stringify(chapterIndex, null, 2));
+
+
+    //write top index every time... lazy
+    topIndex.children.sort();
+    fs.writeFile(outputPath + '/index.json', JSON.stringify(topIndex, null, 2));
 
 
     //create directory and save index file
@@ -100,9 +118,6 @@ function scrape(filePath) {
         fs.mkdirSync(path);
       }
 
-      //save index file
-      fs.writeFile(path + '/index.json', JSON.stringify(index, null, 2));
-
       return path;
     }
 
@@ -110,4 +125,9 @@ function scrape(filePath) {
       fs.writeFile(path + '/' + index.num + '.json', JSON.stringify(index, null, 2));
     }
   });
+
+
+
 }
+
+
